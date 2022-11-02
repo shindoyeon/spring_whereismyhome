5438ca71567bdd82dfe7c104bac04040e4e674a2
dyeon7310 <dyeon7310@gmail.com>
2022-11-02 16:44:06 +0900

    관심지역 등록 및 삭제, 지동 이동 추가

--- src/main/java/com/ssafy/apartment/controller/StarController.java
+++ src/main/java/com/ssafy/apartment/controller/StarController.java
@@ -1,8 +1,9 @@
 package com.ssafy.apartment.controller;
 
 import java.io.IOException;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,12 +14,15 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ssafy.apartment.model.StarDto;
 import com.ssafy.apartment.model.service.StarService;
@@ -38,69 +42,63 @@ public class StarController {
 	private static final Logger logger = LoggerFactory.getLogger(StarController.class);
 	
 
-	@DeleteMapping(value = "deletestar")
-	private String deletestar(HttpServletRequest request, HttpServletResponse response) {
-		HttpSession session = request.getSession();
+	@PostMapping(value = "deletestar")
+	private String deletestar(@RequestParam("starno") String starno, HttpSession session, Model model) {
 		MemberDto memberDto = (MemberDto) session.getAttribute("userinfo");
+		logger.debug("ApartmentController  ! deletestar {}", starno);
 		if(memberDto != null) {
 			try {
-				int starNo = Integer.parseInt(request.getParameter("starno"));
+				int starNo = Integer.parseInt(starno);
 				starService.deleteStar(starNo);
-				return "/apartment/liststar";
+				return "redirect:/star/liststar";
 			} catch (Exception e) {
 				e.printStackTrace();
-				request.setAttribute("msg", "관심목록 삭제 중 에러발생!!!");
 				return "/error/error.jsp";
 			}
 		} else {
-			return "/user/login.jsp";
+			return "/user/login";
 		}
 	}
 
 	@GetMapping(value="/liststar")
-	private String liststar(Model model) {
-//		HttpSession session = request.getSession();
-//		MemberDto memberDto = (MemberDto) session.getAttribute("userinfo");
-//		if(memberDto != null) {
+	private String liststar(HttpSession session, Model model) {
+		MemberDto memberDto = (MemberDto) session.getAttribute("userinfo");
+		System.out.println(memberDto.getUserId());
 			try {
-				List<StarDto> list = starService.listStar("ssafy");
+				List<StarDto> list = starService.listStar(memberDto.getUserId());
+				logger.debug("{}", list.size());
 				model.addAttribute("stars", list);
 				return "/apartment/liststar";
 			} catch (Exception e) {
 				e.printStackTrace();
-				//request.setAttribute("msg", "관심지역목록 얻기 중 에러발생!!!");
-				return "/error/error";
+				return "/user/login";
 			}
-//		} else {
-//			return "/user/login.jsp";
-//		}
 	}
 
 	@GetMapping(value = "/mvaptapi")
 	public String mvaptapi(Model model) throws Exception {
-		logger.debug("ApartmentController ! mvaptapi  ");
+		logger.debug("ApartmentController  ! mvaptapi  ");
 		return "/apartment/aptapi";
 	}
 	
-	@PostMapping(value="addstar")
-	private String addstar(HttpServletRequest request, HttpServletResponse response) {
-		HttpSession session = request.getSession();
+	@PostMapping(value="/addstar")
+	private String addstar(@RequestParam("dong") String dong, HttpSession session, Model model) {
+		Map<String, String> map = new HashMap<String, String>();
 		MemberDto memberDto = (MemberDto) session.getAttribute("userinfo");
-		if(memberDto != null) {
-			StarDto starDto = new StarDto();
-			starDto.setUserId(memberDto.getUserId());
-			starDto.setDongCode(request.getParameter("dong"));
-			starDto.setDealYM(request.getParameter("year")+request.getParameter("month"));
-			try {
-				starService.addStar(starDto);
-				return "/apartment/liststar";
-			} catch (Exception e) {
-				e.printStackTrace();
-				request.setAttribute("msg", "즐겨찾기 추가 중 에러발생!!!");
-				return "/error/error.jsp";
-			}
-		} else {
-			return "/user/login.jsp";
+		map.put("userId", memberDto.getUserId());
+		map.put("dongCode", dong);
+		map.put("dealYM", "");
+		logger.debug("ApartmentController ! addstar {} ", map);
+		try {
+			starService.addStar(map);
+			return "redirect:/star/liststar";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "/error/error";
 		}
+	}
+	private ResponseEntity<String> exceptionHandling(Exception e) {
+		e.printStackTrace();
+		return new ResponseEntity<String>("Error : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
